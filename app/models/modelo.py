228979b73820c943ea6a3521d89f717cf36d9b4f
(mabittar) <ma_bittar@yahoo.com.br>
2021-03-09 14:27:58 -0300

    folder structure

---
+++ app/models/modelo.py
@@ -0,0 +1,15 @@
+from typing import Optional, List, Dict, Any
+from pydantic import BaseModel
+from app.server.data import StatusItens
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str]
+    image: Optional[str]
+    link: Optional[str]
+    have: StatusItens
+
+
+class ItemResposta(Item):
+    id: int
